e0d83ed54576b1bb3f292130a0f0c8b9beda788f
Sandeep Sekhon <sandeep@moula.com.au>
2017-09-25 15:04:48 +1000

    Re add the steps

--- .teamcity/JavaScript/buildTypes/JavaScript_03DeployToStaging.kt
+++ .teamcity/JavaScript/buildTypes/JavaScript_03DeployToStaging.kt
@@ -1,6 +1,12 @@
 package JavaScript.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v10.*
+import jetbrains.buildServer.configs.kotlin.v10.buildSteps.ScriptBuildStep
+import jetbrains.buildServer.configs.kotlin.v10.buildSteps.ScriptBuildStep.*
+import jetbrains.buildServer.configs.kotlin.v10.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v10.triggers.VcsTrigger
+import jetbrains.buildServer.configs.kotlin.v10.triggers.VcsTrigger.*
+import jetbrains.buildServer.configs.kotlin.v10.triggers.vcs
 
 object JavaScript_03DeployToStaging : BuildType({
     uuid = "17e3de47-f971-4d52-a316-3e3d463a43b8"
@@ -10,6 +16,17 @@ object JavaScript_03DeployToStaging : BuildType({
     vcs {
         root(JavaScript.vcsRoots.JavaScript_HttpsGithubComG0t4teamcityCourseCards)
 
+    }
+
+    steps {
+        script {
+            name = "IIS Deploy"
+            id = "RUNNER_6"
+            scriptContent = """
+            rmdir /S /Q \inetpub\wwwroot
+            xcopy /S /I /Y MyApp \inetpub\wwwroot
+            """
+        }
     }
 
     dependencies {
